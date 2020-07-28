b97d830ab9cbc825771a72a42c232cd356cfbbcd
duoduo <909204012@qq.com>
2020-07-28 09:54:20 +0800

    张铎 2020/07/28 09:54

--- src/main/java/com/hy/traffic/TrafficManagerApplication.java
+++ src/main/java/com/hy/traffic/TrafficManagerApplication.java
@@ -1,13 +1,9 @@
 package com.hy.traffic;
 
-import com.alibaba.fastjson.serializer.SerializerFeature;
-import com.alibaba.fastjson.support.config.FastJsonConfig;
-import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.converter.HttpMessageConverter;
 
 @SpringBootApplication
 public class TrafficManagerApplication {
@@ -16,21 +12,30 @@ public class TrafficManagerApplication {
         SpringApplication.run(TrafficManagerApplication.class, args);
     }
 
-
-
+    /**
+     *   mybatis-plus分页插件
+     */
     @Bean
-    public HttpMessageConverters fastJsonHttpMessageConverters() {
-        //1、定义一个convert转换消息的对象
-        FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
-        //2、添加fastjson的配置信息
-        FastJsonConfig fastJsonConfig = new FastJsonConfig();
-        fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
-        //3、在convert中添加配置信息
-        fastConverter.setFastJsonConfig(fastJsonConfig);
-        //4、将convert添加到converters中
-        HttpMessageConverter<?> converter = fastConverter;
-        return new HttpMessageConverters(converter);
+    public PaginationInterceptor paginationInterceptor() {
+        PaginationInterceptor page = new PaginationInterceptor();
+        page.setDialectType("mysql");
+        return page;
     }
+
+
+//    @Bean
+//    public HttpMessageConverters fastJsonHttpMessageConverters() {
+//        //1、定义一个convert转换消息的对象
+//        FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
+//        //2、添加fastjson的配置信息
+//        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+//        fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
+//        //3、在convert中添加配置信息
+//        fastConverter.setFastJsonConfig(fastJsonConfig);
+//        //4、将convert添加到converters中
+//        HttpMessageConverter<?> converter = fastConverter;
+//        return new HttpMessageConverters(converter);
+//    }
 
 
 }
