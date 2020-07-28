569b631bea189201ad775b533c52e160b2aa8114
duoduo <909204012@qq.com>
2020-07-28 16:21:51 +0800

    张铎 2020/07/28 10:00

--- src/main/java/com/hy/traffic/TrafficManagerApplication.java
+++ src/main/java/com/hy/traffic/TrafficManagerApplication.java
@@ -2,6 +2,7 @@ package com.hy.traffic;
 
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.MultipartConfigFactory;
